8e489933717b41681a37735ccb384fdc2d18daf0
francescorfi <francescorfi@gmail.com>
2016-10-30 01:34:34 +0200

    changed tracks model

--- tracks/models.py
+++ tracks/models.py
@@ -2,7 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+#imported relationships
+from albums.models import Album
+from artists.models import Artist
+
+
 class Track(models.Model):
     title = models.CharField(max_length=255)
     order = models.PositiveIntegerField()
     track_file = models.FileField(upload_to='tracks')
+    album = models.ForeignKey(Album, default=0)
+    artist = models.ForeignKey(Artist, default=0)
